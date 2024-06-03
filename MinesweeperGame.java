993208967e4c05a87b20be9f39049a9d41c8ec57
Drumgun23 <148459863+Drumgun23@users.noreply.github.com>
2024-06-03 11:32:10 +0300

    Add files via upload

---
+++ MinesweeperGame.java
@@ -0,0 +1,62 @@
+package minesweeper;
+import java.util.Random;
+
+public class MinesweeperGame {
+    private final int rows;
+    private final int cols;
+    private final int[][] grid;
+    private final boolean[][] mines;
+
+    public MinesweeperGame(int rows, int cols, int minesCount) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new int[rows][cols];
+        this.mines = new boolean[rows][cols];
+        placeMines(minesCount);
+        calculateNumbers();
+    }
+
+    private void placeMines(int minesCount) {
+        Random rand = new Random();
+        for (int i = 0; i < minesCount; i++) {
+            int r, c;
+            do {
+                r = rand.nextInt(rows);
+                c = rand.nextInt(cols);
+            } while (mines[r][c]);
+            mines[r][c] = true;
+        }
+    }
+
+    private void calculateNumbers() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (!mines[r][c]) {
+                    int count = 0;
+                    for (int dr = -1; dr <= 1; dr++) {
+                        for (int dc = -1; dc <= 1; dc++) {
+                            int rr = r + dr;
+                            int cc = c + dc;
+                            if (rr >= 0 && rr < rows && cc >= 0 && cc < cols && mines[rr][cc]) {
+                                count++;
+                            }
+                        }
+                    }
+                    grid[r][c] = count;
+                }
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return grid[row][col];
+    }
+
+    public boolean hasMine(int row, int col) {
+        return mines[row][col];
+    }
+
+    public boolean revealCell(int row, int col) {
+        return !mines[row][col];
+    }
+}
